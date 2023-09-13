ebef2f1a56072f700f3709effc553d14e4cea9ff
JuanFranciscoGrillo <134559638+JuanFranciscoGrillo@users.noreply.github.com>
2023-09-13 09:42:05 -0500

    Setup user routes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const routes = require('./controllers');
+const path = require('path');
+const routes = require('./controllers/api');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
@@ -21,10 +22,13 @@ app.use(session({
   saveUninitialized: true
 }));
 
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true })); 
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(routes);
+app.use('/api', routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log(`Now listening on http://localhost:${PORT}`));
 });
